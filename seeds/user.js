4e304be4e7939707d37543fd27c01ccfa6a9d00f
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-06 09:47:37 -0700

    trying to get it to work

--- seeds/user.js
+++ seeds/user.js
@@ -1,18 +1,21 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
-const userData = [{
-        username: 'John',
-        password: 'jkohn'
-
-    },
-    {
-        username: 'Jake',
-        password: 'jake'
-    },
-    {
-        username: 'Joe',
-        password: 'joe'
-    }
+const userData = [
+  {
+    username: "John",
+    email: "John@test.com",
+    password: "johntest",
+  },
+  {
+    username: "Jake",
+    email: 'Jake@test.com',
+    password: "jaketest",
+  },
+  {
+    username: "Joe",
+    email: 'Joe@test.com',
+    password: "joestest",
+  },
 ];
 
 const seedUsers = () => User.bulkCreate(userData);
